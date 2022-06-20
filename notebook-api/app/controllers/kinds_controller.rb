0f0b51e30378a3b37101e7e260645c31cd158e60
Filipe Gabriel Rocha <filiperochaprogramador@gmail.com>
2022-06-20 14:26:45 -0300

    andamento do projeto

---
+++ notebook-api/app/controllers/kinds_controller.rb
@@ -0,0 +1,71 @@
+class KindsController < ApplicationController
+
+  include ActionController::HttpAuthentication::Digest::ControllerMethods
+  USER = { "filipe" => Digest::MD5.hexdigest(["filipe", "Application", "senha"].join(":"))}
+  
+  # include ActionController::HttpAuthentication::Basic::ControllerMethods
+  # http_basic_authenticate_with name: "filipe", password: "senha"
+
+  before_action :authenticate
+  before_action :set_kind, only: %i[ show update destroy ]
+
+  # GET /kinds
+  def index
+    @kinds = Kind.all
+
+    render json: @kinds
+  end
+
+  # GET /kinds/1
+  def show
+    render json: @kind
+  end
+
+  # POST /kinds
+  def create
+    @kind = Kind.new(kind_params)
+
+    if @kind.save
+      render json: @kind, status: :created, location: @kind
+    else
+      render json: @kind.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /kinds/1
+  def update
+    if @kind.update(kind_params)
+      render json: @kind
+    else
+      render json: @kind.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /kinds/1
+  def destroy
+    @kind.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_kind
+      if params[:contact_id]
+        kind_id = Contact.find(params[:contact_id]).kind_id
+      else
+        kind_id = Kind.find(params[:id])
+      end
+
+      @kind = Kind.find(kind_id)
+    end
+
+    # Only allow a list of trusted parameters through.
+    def kind_params
+      params.require(:kind).permit(:description)
+    end
+
+    def authenticate
+      authenticate_or_request_with_http_digest("Application") do |username|
+        USERS[username]
+      end
+    end
+end
